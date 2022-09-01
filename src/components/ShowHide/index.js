60bd889801f30c900eafce7998f9001339e870fa
Surya T N <suryatej.nim@yahoo.com>
2022-09-01 08:28:04 +0000

    Implemented ShowHide component in corresponding index.js with conditional rendering and state management with setState method and state property. Added/adjusted elements CSS styling in corresponding index.css file

--- src/components/ShowHide/index.js
+++ src/components/ShowHide/index.js
@@ -1,1 +1,62 @@
-// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+export default class ShowHide extends Component {
+  state = {
+    showFirstName: false,
+    showLastName: false,
+  }
+
+  onShowHideFirstName = () =>
+    this.setState(previousShowHideState => !previousShowHideState.showFirstName)
+
+  onShowHideLastName = () =>
+    this.setState(previousShowHideState => !previousShowHideState.showLastName)
+
+  render() {
+    const {showFirstName, showLastName} = this.state
+
+    return (
+      <div className="show-hide-bg-container">
+        <div className="content-container">
+          <h1 className="content-header">Show/Hide</h1>
+          <div className="show-hide-names-container">
+            <div className="show-hide-single-name-container">
+              <button
+                type="button"
+                className="show-hide-name-button"
+                onClick={this.onShowHideFirstName}
+              >
+                Show/Hide Firstname
+              </button>
+              {showFirstName ? (
+                <div className="name-container">
+                  <p className="name-text">Joe</p>
+                </div>
+              ) : (
+                <span />
+              )}
+            </div>
+
+            <div className="show-hide-single-name-container">
+              <button
+                type="button"
+                className="show-hide-name-button"
+                onClick={this.onShowHideLastName}
+              >
+                Show/Hide Lastname
+              </button>
+              {showLastName ? (
+                <div className="name-container">
+                  <p className="name-text">Jonas</p>
+                </div>
+              ) : (
+                <i />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
